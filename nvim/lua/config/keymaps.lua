68cb5f6f13606e4886d61b3f8c94060a1228ba31
shiyuu <tamemotoyou@gmail.com>
2026-02-08 22:20:47 +0900

    feat: add initial Neovim configuration files and setup

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,17 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap -- for conciseness
+
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search hi1ghlights" })
+
+-- Increment/Decrement numbers
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
+
+-- Window Management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+keymap.set("n", "<leader>sx", "<cmd>Close<CR>", { desc = "Close current split" })
