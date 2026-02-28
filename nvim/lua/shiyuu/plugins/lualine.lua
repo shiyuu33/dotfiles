603536f9fdf126cfe245a95596e969bf195a6b46
shiyuu <tamemotoyou@gmail.com>
2026-02-28 14:02:03 +0900

    feat: update mise configuration and replace bufferline with lualine in Neovim setup

--- nvim/lua/shiyuu/plugins/lualine.lua
+++ nvim/lua/shiyuu/plugins/lualine.lua
@@ -1,12 +1,7 @@
 return {
-    "akinsho/bufferline.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    version = "*",
-    opts = {
-      options = {
-        mode = "tabs",
-        separator_style = "slant",
-      }
-    }
-  }
-  
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup()
+  end
+}
